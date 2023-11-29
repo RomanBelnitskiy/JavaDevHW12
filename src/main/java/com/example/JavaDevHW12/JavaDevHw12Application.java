62ac245d1ff79decf82a3315c293d4db6f49a1f8
Roman Belnitskiy <roman.belnitskiy@gmail.com>
2023-11-29 23:59:37 +0200

    first commit

---
+++ src/main/java/com/example/JavaDevHW12/JavaDevHw12Application.java
@@ -0,0 +1,13 @@
+package com.example.JavaDevHW12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevHw12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevHw12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaDevHW12/JavaDevHw12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaDevHW12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevHw12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
